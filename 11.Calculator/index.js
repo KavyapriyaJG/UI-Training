6096771652e6418547ca03c706796ecf8039974e
Kavyapriya <kavyapriya.govindarajan@cdw.com>
2023-02-27 14:06:36 +0530

    Updated index.js
    Function Comments

--- 11.Calculator/index.js
+++ 11.Calculator/index.js
@@ -1,66 +1,66 @@
 //=========== OBJECT - CALCULATOR ================
 Calculator= {
-        isValid : /**
-                  * Type checking - for checking both parameters are of type number
-                  * @param number value1
-                  * @param number value2
-                  * @return boolean
-                  **/
-                function (value1, value2) {
+    /**
+     * Type checking - for checking both parameters are of type number
+     * @param number value1
+     * @param number value2
+     * @return boolean
+    **/     
+    isValid : function (value1, value2) {
                     return typeof value1=='number' && typeof value2=='number'; 
-                },
+              },
 
-    addition : /**
-                  * Adds two numbers
-                  * @param number value1
-                  * @param number value2
-                  * @return number
-                **/
-                function (value1, value2){
+    /**
+     * Adds two numbers
+     * @param number value1
+     * @param number value2
+     * @return number
+    **/            
+    addition : function (value1, value2){
                     if (this.isValid(value1, value2))    
                         return value1+ value2;
                     else  
                         console.log("Enter 2 valid numbers"); 
-                },
-        
-    subtraction : /**
-                  * Subtracts two numbers
-                  * @param number value1
-                  * @param number value2
-                  * @return number
-                  **/
-                  function (value1, value2){
-                      if (this.isValid(value1, value2))   
-                          return value1- value2;
-                      else  
-                          console.log("Enter 2 valid numbers");
+               },
+    
+    /**
+     * Subtracts two numbers
+     * @param number value1
+     * @param number value2
+     * @return number
+    **/              
+    subtraction : function (value1, value2){
+                        if (this.isValid(value1, value2))   
+                            return value1- value2;
+                        else  
+                            console.log("Enter 2 valid numbers");
                   },
 
-    multiplication : /**
-                      * Multiplies two numbers
-                      * @param number value1
-                      * @param number value2
-                      * @return number
-                      **/
-                      function (value1, value2){
-                          if (this.isValid(value1, value2))  
-                              return value1* value2;
-                          else  
-                              console.log("Enter 2 valid numbers");
-                      },
+    /**
+     * Multiplies two numbers
+     * @param number value1
+     * @param number value2
+     * @return number
+    **/          
+    multiplication : function (value1, value2){
+                        if (this.isValid(value1, value2))  
+                            return value1* value2;
+                        else  
+                            console.log("Enter 2 valid numbers");
+                     },
 
-    division : /**
-                  * Divides two numbers
-                  * @param number value1
-                  * @param number value2
-                  * @return number
-                **/
-                function (value1, value2){
+    /**
+     * Divides two numbers
+     * @param number value1
+     * @param number value2
+     * @return number
+    **/     
+    division : function (value1, value2){
                     if (this.isValid(value1, value2))    
                         return value1/ value2;
                     else  
                         console.log("Enter 2 valid numbers");
-                }
+               }
 
 }
 
@@ -70,7 +70,8 @@ number2 = 10;
 console.log("CALCULATOR");
 
 
-console.log(number1 + " + " + number2 + " = " + Calculator.addition(number1, number2) + "\n");
-console.log(number1 + " - " + number2 + " = " + Calculator.subtraction(number1, number2) + "\n");
-console.log(number1 + " * " + number2 + " = " + Calculator.multiplication(number1, number2) + "\n");
-console.log(number1 + " / " + number2 + " = " + Calculator.division(number1, number2) + "\n");
+console.log(number1 + " + " + number2 + " = " + Calculator.addition(number1, number2));
+console.log(number1 + " - " + number2 + " = " + Calculator.subtraction(number1, number2));
+console.log(number1 + " * " + number2 + " = " + Calculator.multiplication(number1, number2));
+console.log(number1 + " / " + number2 + " = " + Calculator.division(number1, number2));
+
